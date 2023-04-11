7077fa38f0d201d79331d518a586eb5bd90d3021
amancoditas <123372398+amancoditas@users.noreply.github.com>
2023-04-12 02:06:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ tasks = [
     {
         'id': 1,
         'title': 'Task 1',
-        'description': 'This is task 1' "AMAN ARYAMAN",
+        'description': 'This is task 1' "AMAN CICD Pipeline Done",
         'done': False
     },
     {
